da8885b3377357d9d535f141d545802d5e73ecf0
Rasmusmario123 <84456427+Rasmusmario123@users.noreply.github.com>
2025-04-05 09:03:08 +0200

    Player tag and funky raycasting

--- 🍄/Assets/Scripts/PlayerController.cs
+++ 🍄/Assets/Scripts/PlayerController.cs
@@ -20,7 +20,20 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 inputAxis = new Vector2(Input.GetAxisRaw("Vertical"), Input.GetAxisRaw("Horizontal"));
 
-        Debug.Log(inputAxis);
+        //Debug.Log(inputAxis);
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+        {
+            Debug.Log(hit.distance);
+            Debug.Log("This is the math: " + transform.position.y + (0.5f - hit.distance));
+            if (hit.distance < 0.5f)
+            {
+                transform.position.Set(transform.position.x, transform.position.y + (0.5f - hit.distance), transform.position.z);
+            }
+        }
+
     }
 
 
