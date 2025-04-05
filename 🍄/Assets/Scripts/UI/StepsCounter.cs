62d8c52550a5afe897ee289bc2ce7f53f25a72da
Måns Fritiofsson <mans@fritiofson.net>
2025-04-05 10:10:20 +0200

    Simple steps counter

---
+++ 🍄/Assets/Scripts/UI/StepsCounter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StepsCounter : MonoBehaviour
+{
+    [SerializeField] IntReference StepsRef;
+    [SerializeField] TextMeshProUGUI Label;
+
+    void Awake()
+    {
+        StepsRef.OnValueChanged += RemainingStepsUpdated;
+    }
+
+    void RemainingStepsUpdated()
+    {
+        Label.text = $"Steps remaining: {StepsRef.Value}";
+    }
+}
