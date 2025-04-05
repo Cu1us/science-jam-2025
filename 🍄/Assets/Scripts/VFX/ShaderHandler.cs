66b39cfd837c1a8bff937c379dfe47bd62793f00
Måns Fritiofsson <mans@fritiofson.net>
2025-04-05 14:43:39 +0200

    Pixel shader finally

---
+++ 🍄/Assets/Scripts/VFX/ShaderHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShaderHandler : MonoBehaviour
+{
+    // [SerializeField] Material Shader;
+
+    // void OnRenderImage(RenderTexture src, RenderTexture dest)
+    // {
+    //     Graphics.Blit(src, dest, Shader, pass: 0);
+    // }
+}
