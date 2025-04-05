0e770edae0bd4453ef4de2c3bdf497f4a8613f84
Måns Fritiofsson <mans@fritiofson.net>
2025-04-05 08:56:58 +0200

    Interactor collider & interaction events

--- 🍄/Assets/Scripts/InteractorCollider.cs
+++ 🍄/Assets/Scripts/InteractorCollider.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class InteractorCollider : MonoBehaviour
 {
     List<Interactable> InteractablesInRange = new();
+    Interactable SelectedInteractable = null;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out Interactable interactable))
         {
             InteractablesInRange.Add(interactable);
+            SelectClosestInteractable();
         }
     }
 
@@ -17,22 +20,41 @@ public class InteractorCollider : MonoBehaviour
     {
         if (other.TryGetComponent(out Interactable interactable))
         {
-            InteractablesInRange.Add(interactable);
+            InteractablesInRange.Remove(interactable);
+            SelectClosestInteractable();
         }
     }
 
-    void ShowButtonPromptOnClosestInteractable()
+    void SelectClosestInteractable()
     {
-        Interactable closestInteractable;
+        Interactable closestInteractable = null;
         float closestSqrDistance = 0;
         foreach (Interactable interactable in InteractablesInRange)
         {
+            interactable.SetButtonPromptVisible(false);
             float sqrDistance = Vector3.SqrMagnitude(interactable.transform.position - transform.position);
             if (sqrDistance < closestSqrDistance)
             {
                 closestInteractable = interactable;
                 closestSqrDistance = sqrDistance;
             }
+        }
+        if (closestInteractable)
+        {
+            closestInteractable.SetButtonPromptVisible(true);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && SelectedInteractable != null)
+        {
+            SelectedInteractable.InteractEnd();
+        }
+        if (Input.GetButtonUp("Interact") && SelectedInteractable != null)
+        {
+            SelectedInteractable.InteractStart();
+            SelectClosestInteractable();
         }
     }
 }
