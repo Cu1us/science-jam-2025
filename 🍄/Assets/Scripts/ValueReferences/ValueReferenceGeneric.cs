7b9b8edff290400b0b65cabecdc8a1031b16acbc
Måns Fritiofsson <mans@fritiofson.net>
2025-04-05 10:09:42 +0200

    Value reference tracking & default values

--- 🍄/Assets/Scripts/ValueReferences/ValueReferenceGeneric.cs
+++ 🍄/Assets/Scripts/ValueReferences/ValueReferenceGeneric.cs
@@ -1,8 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class ValueReferenceGeneric<T> : ScriptableObject
 {
-    public T Value;
+    [SerializeField] protected T _value;
+    public T Value
+    {
+        get
+        {
+            return _value;
+        }
+        set
+        {
+            _value = value;
+            OnValueChanged?.Invoke();
+        }
+    }
+    [SerializeField] private T DefaultValue;
+
+    public Action OnValueChanged;
+
+    private void OnEnable()
+    {
+#if UNITY_EDITOR
+        EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+#endif
+    }
+
+#if UNITY_EDITOR
+    private void OnPlayModeStateChanged(PlayModeStateChange obj)
+    {
+        switch (obj)
+        {
+            case PlayModeStateChange.EnteredEditMode:
+                OnValueChanged = null;
+                _value = DefaultValue;
+                break;
+
+            case PlayModeStateChange.ExitingEditMode:
+                OnValueChanged = null;
+                break;
+        }
+    }
+#endif
 }
