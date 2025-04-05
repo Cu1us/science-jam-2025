26e25e82344b9e3dc6aa047dc458ec384f916eff
wWinklepicker <sarahdove2005@gmail.com>
2025-04-05 13:59:36 +0200

    Added dialogue System

--- 🍄/Assets/Scripts/DialogueManager.cs
+++ 🍄/Assets/Scripts/DialogueManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
     [SerializeField] Dialogue gameDialogue;
+
+    [SerializeField] TextMeshProUGUI textElement;
     private Conversation currentConversation;
-    public void loadConversation(int i)
+    private bool conversing = false;
+    private int step = 0;
+
+    public void StartConversation(int i)
     {
+        textElement.gameObject.SetActive(true);
+        step = 0;
+        conversing = true;
         currentConversation = gameDialogue.gameDialogue[i];
+        textElement.text = currentConversation.dialogueBoxes[step].dialogueText;
+    }
+    private void Start()
+    {
+        StartConversation(0);
+    }
+    private void Update()
+    {
+        if (!conversing)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            step++;
+            if (step >= currentConversation.dialogueBoxes.Length) 
+            {
+                conversing = false;
+                textElement.gameObject.SetActive(false);
+                return;
+            }
+            UpdateDialogueBox(currentConversation.dialogueBoxes[step]);
+        }
     }
 
-    public void startConversation()
+    private void UpdateDialogueBox(DialogueBox d)
     {
-
+        textElement.text = d.dialogueText;
     }
 }
