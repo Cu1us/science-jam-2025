08b53f9ccbef1b2a826f793a3c0f0f7753c656e7
wWinklepicker <sarahdove2005@gmail.com>
2025-04-05 15:45:07 +0200

    Camera updated and fixed

--- 🍄/Assets/Scripts/CameraController.cs
+++ 🍄/Assets/Scripts/CameraController.cs
@@ -10,14 +10,12 @@ public class CameraController : MonoBehaviour
 
     [SerializeField] private float horisontalBorder;
     [SerializeField] private float verticalBorder;
-    [SerializeField] private float depthBorder;
 
     [SerializeField] private float cameraSpeed;
 
-    private Vector3 cameraVector;
-
+    private Vector3 offset;
+    private Vector3 yAxis = new Vector3();
     private Transform target;
-    private Vector3 targetPosition;
 
     void Start()
     {
@@ -25,34 +23,34 @@ public class CameraController : MonoBehaviour
 
         var height = Mathf.Sin(cameraAngle * (Mathf.PI / 180)) * cameraDistance;
         var length = Mathf.Cos(cameraAngle * (Mathf.PI / 180)) * cameraDistance;
-        cameraVector = new Vector3(0, height, length * (-1));
+        offset = new Vector3(0, height, length * (-1));
         transform.localPosition = target.transform.position;
-        transform.localPosition += cameraVector;
+        transform.localPosition += offset;
+        offset *= -1;
         transform.rotation = Quaternion.Euler(cameraAngle, 0, 0);
-        cameraVector.Normalize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        targetPosition = gameObject.GetComponent<Camera>().WorldToViewportPoint(target.position);
-        targetPosition.x -= .5f; targetPosition.y -= .5f; targetPosition.z -= cameraDistance;
         MoveCamera();
     }
 
     void MoveCamera()
     {
-        if (Mathf.Abs(targetPosition.x) > horisontalBorder)
+        Vector3 tempVec = (transform.position + offset) - target.position;
+        if (Mathf.Abs(tempVec.z) > horisontalBorder)
         {
-            transform.position += MathF.Pow(targetPosition.x, 5) * transform.right * cameraSpeed * Time.deltaTime;
+            transform.position += -1 * MathF.Pow(tempVec.z, 5) * Vector3.forward * cameraSpeed * Time.deltaTime;
         }
-        if (Mathf.Abs(targetPosition.y) > verticalBorder)
+        if (Mathf.Abs(tempVec.x) > verticalBorder)
         {
-            transform.position += MathF.Pow(targetPosition.y, 5) * transform.up * cameraSpeed * Time.deltaTime;
+            transform.position +=  -1 * MathF.Pow(tempVec.x, 5) * Vector3.right * cameraSpeed * Time.deltaTime;
         }
-        if (Mathf.Abs(targetPosition.z) > depthBorder)
-        {
-            transform.position += (MathF.Pow(targetPosition.z, 5) * transform.forward * Time.deltaTime);
-        }
+        yAxis.x = transform.position.x;
+        yAxis.y = -1 * offset.y + target.position.y;
+        yAxis.z = transform.position.z;
+
+        transform.position = yAxis;
     }
 }
